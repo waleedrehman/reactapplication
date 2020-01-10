5e7b0d0bf08da5d4b58706e89890f33a0f0f9150
Waleed Ur Rehman <mac@255.sub-97-9-191.myvzw.com>
2020-01-10 12:01:45 +0000

    Added in the permutation solution and explaining

--- src/components/Permutation.jsx
+++ src/components/Permutation.jsx
@@ -57,7 +57,99 @@ public class PermutationDTO {
 }
             `}
             </Highlight>
-            
+            <p>The purpose of the DTO (Data Transfer Object) is to carry the data from the point it is created to the end of the process, in this instance it would be used to return the data from the server back to the user no matter where they generate the request from. As part of this solution I also have the Rest Controller for the Permutation calls which gets the permutation request and handles it:</p>
+            <Highlight language='java'>
+{`
+package uk.co.waleed.Permutation.Controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import uk.co.waleed.Permutation.Model.PermutationDTO;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class PermutationController {
+
+    private static final String template = "The permuted value is, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @CrossOrigin(origins = "*")
+    @RequestMapping("/permutation")
+    public PermutationDTO permutation(@RequestParam(value="permuteInt", defaultValue="326") String permuteInt) {
+        ArrayList<Integer> listOfNumbers = new ArrayList<Integer>();
+
+        for(char ch : permuteInt.toCharArray()){
+            if(Character.isDigit(ch)){
+                listOfNumbers.add(Character.getNumericValue(ch));
+            }
+        }
+
+        if (listOfNumbers == null || listOfNumbers.isEmpty()){
+            //throw new NumberFormatException("The input entered doesn't have any digits " + permuteInt);
+            return new PermutationDTO(counter.incrementAndGet(),
+                    String.format(template, "The input entered doesn't have any digits '" + permuteInt + "'"));
+        }
+
+        System.out.println("You entered "+ permuteInt);
+        System.out.println("The digits in the input are " + listOfNumbers.toString());
+        ArrayList<String> result = new ArrayList<String>();
+        permute(listOfNumbers.toArray(),0,result);
+        //if we get 123 the result should be 123,132,213,231,312,321
+        String res = "";
+        Collections.sort(result, Collections.reverseOrder());
+        for (int i = 0; i<result.size();i++){
+            if (i == result.size()-1){
+                res += result.get(i);
+            }
+            else {
+                res += result.get(i) + ", ";
+            }
+        }
+        System.out.println(res);
+        //setResult(res);
+        System.out.println("Thanks and goodbye");
+
+        return new PermutationDTO(counter.incrementAndGet(),
+                            String.format(template, res));
+    }
+
+    private static void permute(Object[] values, int index, ArrayList<String> res)
+    {
+        String result = "";
+        Object[] num = Arrays.copyOf(values, values.length);
+        if(index==num.length-1)
+        {
+            for(int j=0;j<num.length;j++)
+            {
+                result += num[j];
+            }
+            res.add(result);
+        }
+        else
+        {
+            for(int i=index;i<num.length;i++)
+            {
+                int temp;
+                temp= (int) num[i];
+                num[i]=num[index];
+                num[index]=temp;
+
+                permute(num,index+1,res);
+            }
+        }
+    }
+}
+`}
+            </Highlight>
+            <p>The rest controller gets a string which gets passed to the permutation end point, this could be through Curl command or the React website, Once the input is recieved the controller loops through the characters to see if there are any digits in the string passed in, if there are digits then we are happy with the input and we can just extract the digits as our input for permute, if there are no digits in the input provided we just send a message back to the user as we need digits to be able to do the permutation.</p>
+            <p>Once we are happy with the input we build an array of possible outcomes based on the digits passed in as part of the input. We then send the possible outcomes back to the user.</p>
+            <p>In both of these instance we are using the DTO to send the data back to the user.</p>
             <p>Try it for yourself bellow:</p>
             <form onSubmit={this.permute}>
               <input name="input"></input><button>Permute</button>
